5c09066d514ec96cedb12c28c349313b671004c4
DebaRobot <71641088+DebaRobot@users.noreply.github.com>
2021-07-05 14:32:25 +0530

    Update ShapeDetection.py

--- ShapeDetection.py
+++ ShapeDetection.py
@@ -1,35 +1,42 @@
-from ShapeDetection import ShapeDetector
-import imutils
 import cv2
 
-image = cv2.imread("C:/Users/h22de/OneDrive/Desktop/Solution/shapesPhoto.png")
-resized = imutils.resize(image, width=300)
-ratio = image.shape[0] / float(resized.shape[0])
+class ShapeDetector:
+    def __init__(self):
+        pass
 
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)S
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    def detect(self, c):
+        # initialize the shape name and approximate the contour
+        shape = "unidentified"
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
-thresh1,thresh2 = cv2.threshold(blurred, 210, 255, cv2.THRESH_BINARY_INV)
+        # if the shape is a triangle, it will have 3 vertices
+        if len(approx) == 3:
+            shape = "Triangle"
+            
+        elif len(approx) == 10:
+            shape = "Star"
 
-#cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-cnts = cv2.findContours(thresh2.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # if the shape has 4 vertices, it is either a square or
+        # a rectangle
+        elif len(approx) == 4:
+            # compute the bounding box of the contour and use the
+            # bounding box to compute the aspect ratio
+            (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
 
-cnts = imutils.grab_contours(cnts)
-sd = ShapeDetector()
+            # a square will have an aspect ratio that is approximately
+            # equal to one, otherwise, the shape is a rectangle
+            shape = "Square" if ar >= 0.95 and ar <= 1.05 else "Rectangle"
 
-# loop over the contours
-for c in cnts:
-    M = cv2.moments(c)
-    cX = int((M["m10"] / M["m00"]) * ratio)
-    cY = int((M["m01"] / M["m00"]) * ratio)
-    shape = sd.detect(c)
-    c = c.astype("float")
-    c *= ratio
-    c = c.astype("int")
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-    
-cv2.destroyAllWindows()
+        # if the shape is a pentagon, it will have 5 vertices
+        elif len(approx) == 5:
+            shape = "pentagon"
+
+        # otherwise, we assume the shape is a circle
+        else:
+            shape = "circle"
+
+        # return the name of the shape
+        return shape
